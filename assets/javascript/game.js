1915fd54da096dbc9704bc65f297f3f334e75c5c
Louis Rodriguez <louisp285@yahoo.com>
2019-03-21 15:53:34 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,18 +17,18 @@ function resetGame() {
 
 
     // Math.floor to round down to nearest integer//
-    wordIndex = Math.floor(Math.random() * (listOfWords.length));
+    guessingWordIndex = Math.floor(Math.random() * (listOfWords.length));
 
     // Clear out arrays//
     usedLetters = [];
     guessingWord = [];
 
     //build the guessing word and clear it out//
-    for (var i=0; i < listOfWords[wordIndex].length; i++) {
+    for (var i=0; i < listOfWords[guessingWordIndex].length; i++) {
         guessingWord.push('_');
     }
 
-    document.getElementById('pressKeyTryAgain').style.cssText= 'display: none';
+    document.getElementById('pressKeyTryAgain').style.cssText = 'display: none';
     document.getElementById('you_lose').style.cssText = 'display: none';
     document.getElementById('win_image').style.cssText = 'display: none';
     
@@ -39,50 +39,24 @@ function resetGame() {
 
 
     document.getElementById('totalWins').innerText = wins;
-    document.getElementById('guessingWord').innerText = '';
+    document.getElementById('guessingWord').innerText = '_';
+    var guessingWordText = '';
     for (var i = 0; i < guessingWord.length; i++) {
-        document.getElementById('guessingWord').innetText += guessingWord[i];
-    }
+    document.getElementById('guessingWord').innerText += guessingWord[i]; 
+    guessingWordText += guessingWord[i];
+}
+    document.getElementById('guessingWord').innerText = guessingWordText;
     document.getElementById('remainingGuesses').innerText = remainingGuesses;
     document.getElementById('usedLetters').innerText = usedLetters;
-    if (remainingGuesses <= 0) {
-        document.getElementById('you_lose').style.cssText = 'display: block';
-        document.getElementById('pressKeyTryAgain').style.cssText = 'display: block';
-        hasFinished = true;
     }
-};
-
-    document.onkeydown = function(event) {
-    //once the game has finished, this resets with the pressing of any key.  keystroke is null//
-        if (hasFinished) {
-            resetGame();
-            hasFinished = false;
-        } else {
-            //a-z was pressed?//
-        if(event.keyCode > 65 && event.keyCode <= 90) {
-            makeGuess(event.key.toLowerCase());
-        }
-    }
-};
-
-function makeGuess(letter) {
-    if (remainingGuesses > 0) {
-        if (!gameStarted) {
-            gameStarted = true;
-        }
-        
-        //checking the letter to see if it has been used//
-        if (usedLetters.indexOf(letter) === -1) {
-            usedLetters.push(letter);
-            evaluateGuess(letter);
-        }
-    }
-    updateDisplay();
-    checkWin();
-};
+    // if(remainingGuesses <= 0) {
+    //     document.getElementById('you_lose').style.cssText = 'display: block';
+    //     document.getElementById('pressKeyTryAgain').style.cssText = 'display: block';
+    //     hasFinished = true;
+    // }
 
 function evaluateGuess(letter) {
-    //array to store o[position of letter in string//
+    //array to store position of letter in string//
     var positions = [];
     
     for (var i = 0; i < listOfWords[guessingWordIndex].length; i++) {
@@ -91,16 +65,14 @@ function evaluateGuess(letter) {
         }
     }
 
-
+    // removing guesses //
 if (positions.length <= 0) {
     remainingGuesses--;
     } else {
         for(var i = 0; i < positions.length; i++) {
             guessingWord[positions[i]] = letter;
-
-        }
     }
-};
+}
 
 function checkWin() {
     if(guessingWord.indexOf('') === -1) {
@@ -111,8 +83,40 @@ function checkWin() {
     }
 };
 
+    // Check for a loss //
+    function checkLoss () {
+        if(remainingGuesses <= 0) {
+            document.getElementById('you_lose').style.cssText = "display: block";
+            document.getElementById('pressKeyTryAgain').style.cssText = "display:block";
+            hasFinished = true;
+        }  
+}
+    
 
+    // Make a guess //
+function makeGuess(letter) {
+    if (remainingGuesses > 0) {  
+        //checking the letter to see if it has been used//
+        if (usedLetters.indexOf(letter) === -1) {
+            usedLetters.push(letter);
+            evaluateGuess(letter);
+        }
+    }
+    
+};
 
-
-
-
+// Event listener //
+        document.onkeydown = function(event) {
+            //once the game has finished, this resets with the pressing of any key.  keystroke is null//
+                if (hasFinished) {
+                    resetGame();
+                    hasFinished = false;
+                } else {
+                    //a-z was pressed?//
+                if(event.keyCode >= 65 && event.keyCode <= 90) {
+                    makeGuess(event.key.toLowerCase());
+                    updateDisplay();
+                    checkWin();
+                    checkLoss();
+                }
+            }
